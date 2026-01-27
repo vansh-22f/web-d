4e9c86412d8230ee18b88288667297516ee1036d
Vansh-22f300 <22f3001851@ds.study.iitm.ac.in>
2026-01-27 11:47:10 +0530

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("vansh")
+console.log(Array.isArray("vansh"))
+console.log(Array.from("vansh"))
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1, score2, score3))
