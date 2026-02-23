65ac1a2c39482c5c08ce63a56d11f2dad63befa8
Vansh-22f <vanshmittal.compro@gmail.com>
2026-02-23 15:00:58 +0530

    JS ppt

---
+++ JS/L30 ppt/gec.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+console.log(a);
+
+var a = 10;
+
+function greet() {
+  console.log("Hello");
+}
+
+
+
+
+
+// Explain step-by-step:
+
+// During Creation Phase:
+
+// a is created in memory → value undefined
+
+// greet function is fully stored in memory
+
+// During Execution Phase:
+
+// console.log(a) → prints undefined
+
+// a is assigned 10
+
+// That is why hoisting happens.
