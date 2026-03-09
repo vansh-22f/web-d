d1c3c543aef08badf935c41d3b3c8b1181fe3e35
Vansh-22f <vanshmittal.compro@gmail.com>
2026-03-09 17:49:49 +0530

    vue-6

---
+++ blog-post/src/components/SinglePost.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="post">
+    <h2>{{ post.title }}</h2>
+    <!-- <p>{{ post.body }}</p> -->
+    <p>{{ snippet }}</p>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  props: ["post"],
+  setup(props) {
+    const snippet = computed(() => {
+      return props.post.body.substring(0, 80) + "...";
+    });
+
+    return { snippet };
+  },
+};
+</script>
--- blog-post/src/views/AboutView.vue
+++ blog-post/src/views/AboutView.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+
+    <PostList :posts="posts" />
+
   </div>
 </template>
+
+<script>
+import {ref } from 'vue'
+import PostList from '../components/PostList.vue'
+export default{
+  name:'AboutView',
+
+  components:{PostList},
+
+  setup(){
+    const posts=ref([
+      {id:1,title:"Post 1",body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+      {id:2,title:"Post 2",body:"This is the content of post 2"},
+      {id:3,title:"Post 3",body:"This is the content of post 3"},
+    ]);
+
+
+  
+
+    return {posts}
+  }
+}
+
+
+
+
+
+
+</script>
