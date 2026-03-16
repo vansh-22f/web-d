074ca91537646f383f257aa098bb14abafe199e7
Vansh-22f <vanshmittal.compro@gmail.com>
2026-03-16 11:11:31 +0530

    route-parameters

---
+++ nuxt-app/app/pages/products/[id].vue
@@ -0,0 +1,14 @@
+<!-- /products/:id -->
+
+<template>
+  <div>
+    <p>Product Details for {{ id  }}</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est recusandae accusantium quaerat nam impedit quo temporibus quidem voluptas, laudantium quia rem repellendus architecto quisquam molestiae corrupti, hic corporis mollitia. Culpa, error fuga minus impedit distinctio odio facere amet veniam, doloribus dolor voluptatum tempore assumenda. Veniam non beatae debitis at quas!</p>
+  </div>
+</template>
+
+<script setup>
+
+const {id} =useRoute().params
+
+</script>
